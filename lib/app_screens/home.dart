1a76e5f98ee03f5db821879c361f33a556fe1743
ungsochea <sochea.ung@gmail.com>
2019-08-21 14:47:52 +0700

    Column and Expanded Widgets

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -5,21 +5,67 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Container(
+        padding: EdgeInsets.only(left: 10.0,top: 40.0),
         alignment: Alignment.center,
         color: Colors.deepPurple,
-        // width: 200.0,
-        // height: 100.0,
-        margin: EdgeInsets.only(top:30.0),
-        child: Text("Flight",
-          textDirection: TextDirection.ltr,
-          style: TextStyle(
-            fontSize: 75.0,
-            fontFamily: 'Raleway',
-            fontWeight: FontWeight.w700,
-            color: Colors.white,
-            decoration: TextDecoration.none
-            ),
-        ),
-      ),);
+        child: Column(
+          children: <Widget>[
+            Row(children: <Widget>[
+              Expanded(child:  Text("Spice Jet",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    fontSize: 35.0,
+                    fontFamily: 'Raleway',
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white,
+                    decoration: TextDecoration.none
+                    ),
+                ),
+              ),
+              Expanded(
+                child:  Text("From Mumbai to Bangalore vie New Delhi",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    fontSize: 35.0,
+                    fontFamily: 'Raleway',
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white,
+                    decoration: TextDecoration.none
+                    ),
+                ),
+              )
+            ],
+          ),
+          Row(children: <Widget>[
+              Expanded(child:  Text("Spice Jet",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    fontSize: 35.0,
+                    fontFamily: 'Raleway',
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white,
+                    decoration: TextDecoration.none
+                    ),
+                ),
+              ),
+              Expanded(
+                child:  Text("From Mumbai to Bangalore vie New Delhi",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    fontSize: 35.0,
+                    fontFamily: 'Raleway',
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white,
+                    decoration: TextDecoration.none
+                    ),
+                ),
+              )
+            ],
+          ),
+          ],
+        )
+        
+      ),
+      );
   }
 }
