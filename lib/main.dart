9a05272a82a25ac564fd995c7b6baea98096d379
ungsochea <sochea.ung@gmail.com>
2019-08-21 17:03:20 +0700

    Long List View , list items

--- lib/main.dart
+++ lib/main.dart
@@ -12,29 +12,26 @@ void main(){
   ));
 }
 
+List<String> getListElements(){
+  var items = List<String>.generate(1000, (counter)=>"Item $counter");
+  return items;
+}
+
 Widget getListView(){
-  var listView = ListView(
-    children: <Widget>[
-      ListTile(
-        leading: Icon(Icons.landscape),
-        title: Text("Landscape"),
-        subtitle: Text("Beautiful View !"),
-        trailing: Icon(Icons.wb_sunny),
+  
+  var listItems = getListElements();
+
+  var listView = ListView.builder(
+    itemBuilder: (context,index){
+      return ListTile(
+        leading: Icon(Icons.arrow_right),
+        title: Text(listItems[index]),
         onTap: (){
-          debugPrint("Lanscape tapped");
+          debugPrint('${listItems[index]} was tapped');
         },
-      ),
-      ListTile(
-        trailing: Icon(Icons.laptop_chromebook),
-        title: Text("Windows"),
-      ),
-      ListTile(
-        trailing: Icon(Icons.phone),
-        title: Text("Phone"),
-      ),
-      Text("Yet another element is list"),
-      Container(color: Colors.red,height: 50.0,)
-    ],
+      );
+    }
   );
+
   return listView;
 }
