81fe0277c7c8f602dde9156778c74ec2dade06a4
ungsochea <sochea.ung@gmail.com>
2019-08-21 10:58:11 +0700

    First Screen Class

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import './app_screens/first_screen.dart';
 
-void main(){
-  runApp(
-    Center(
-      child: Text(
-        "Hello Flutter",
-        textDirection: TextDirection.ltr,
+void main()=>runApp(new MyFlutterApp());
+
+class MyFlutterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "My Flutter App",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Text First App Screen"),
+        ),
+        body: FirstScreen(),
       ),
-    )
-  );
+    );
+  }
 }
